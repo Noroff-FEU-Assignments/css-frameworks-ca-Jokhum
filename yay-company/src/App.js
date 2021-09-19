72b190f902fb03c7bd2dc3a48fe01745302d9ca6
Jokhum <p.werven@gmail.com>
2021-09-19 16:05:53 +0200

    Organizing

--- yay-company/src/App.js
+++ yay-company/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./sass/style.scss";
-import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
